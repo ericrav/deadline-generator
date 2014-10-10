d8a69f65682168d09d35d8db0492eef639a362ff
ericrav <ericrabinowitz@me.com>
2014-10-10 00:48:27 -0400

    added angular, figuring out event creation through gapi

--- script.js
+++ script.js
@@ -2,49 +2,81 @@ var clientId = '658261560622-ddrg4ivsu92qda9u4i1cnanid965sscd.apps.googleusercon
 var apiKey = 'AIzaSyB3zI1irAMQdxGjf5uwMcz4T14jPPwb_kk';
 var scopes = 'https://www.googleapis.com/auth/calendar';
 
+angular.module('dgApp', []);
+function MainCtrl($scope) {
 
-// src: http://googleappsdeveloper.blogspot.com/2011/12/using-new-js-library-to-unlock-power-of.html
-function handleClientLoad() {
-    gapi.client.setApiKey(apiKey);
-    window.setTimeout(checkAuth,1);
-    checkAuth();
-}
+    $scope.calendars = []; // array for users calendars
 
-function checkAuth() {
-    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
-        handleAuthResult);
-}
-
-function handleAuthResult(authResult) {
-    var authorizeButton = document.getElementById('authorize-button');
-    if (authResult) {
-        authorizeButton.style.visibility = 'hidden';
-        makeApiCall();
-    } else {
-        authorizeButton.style.visibility = '';
-        authorizeButton.onclick = handleAuthClick;
+    // src: http://googleappsdeveloper.blogspot.com/2011/12/using-new-js-library-to-unlock-power-of.html
+    function handleClientLoad() {
+        gapi.client.setApiKey(apiKey);
+        window.setTimeout(checkAuth,1);
+        checkAuth();
     }
-}
 
-function handleAuthClick(event) {
-    gapi.auth.authorize(
-        {client_id: clientId, scope: scopes, immediate: false},
-        handleAuthResult);
-    return false;
-}
+    function checkAuth() {
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+            handleAuthResult);
+    }
 
-function makeApiCall() {
-  gapi.client.load('calendar', 'v3', function() {
-    var request = gapi.client.calendar.events.list({
-      'calendarId': 'primary'
-    });
-          
-    request.execute(function(resp) {
-      for (var i = 0; i < resp.items.length; i++) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(resp.items[i].summary));
-        document.getElementById('events').appendChild(li);
-      }
-    });
-  });
+    function handleAuthResult(authResult) {
+        var authorizeButton = document.getElementById('authorize-button');
+        if (authResult) {
+            authorizeButton.style.visibility = 'hidden';
+            makeApiCall();
+        } else {
+            authorizeButton.style.visibility = '';
+            authorizeButton.onclick = handleAuthClick;
+        }
+    }
+
+    $scope.handleAuthClick = function(event) {
+        gapi.auth.authorize(
+            {client_id: clientId, scope: scopes, immediate: false},
+            handleAuthResult);
+        return false;
+    }
+
+    $scope.createEvent = function() {
+        console.log($scope.title);
+        console.log($scope.calendar);
+        console.log($scope.date);
+        var data = {
+            "summary": $scope.title,
+            "end": {
+                "date": $scope.date
+            },
+            "start": {
+                "date": $scope.date
+            },
+            "source": {
+                "url": "http://ericrav.github.io/deadline-generator/"
+            }
+        };
+        gapi.client.load('calendar', 'v3', function() {
+            var request = gapi.client.calendar.events.insert({
+                "calendarId": $scope.calendar,
+                "resource": data
+            });
+            request.execute(function(resp) {
+                console.log(resp);
+                // console.log($scope.calendars);
+                // $scope.$apply();
+            });
+        });
+    }
+
+    function makeApiCall() {
+        gapi.client.load('calendar', 'v3', function() {
+            var request = gapi.client.calendar.calendarList.list();
+            request.execute(function(resp) {
+                for (var i = 0; i < resp.items.length; i++) {
+                    var c = resp.items[i]; // calendar object
+                    if (c.accessRole == 'owner' || c.accessRole == 'writer') $scope.calendars.push(c); // only add if user has write priveleges
+                }
+                console.log($scope.calendars);
+                $scope.$apply();
+            });
+        });
+    }
 }
