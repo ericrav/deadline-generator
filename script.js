6e378250dadf73a73976e6d443dc74706e5873a6
ericrav <ericrabinowitz@me.com>
2014-10-14 00:24:22 -0400

    lots of progress!

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var clientId = '658261560622-ddrg4ivsu92qda9u4i1cnanid965sscd.apps.googleusercon
 var apiKey = 'AIzaSyB3zI1irAMQdxGjf5uwMcz4T14jPPwb_kk';
 var scopes = 'https://www.googleapis.com/auth/calendar';
 
+// add function to get date by adding days to date object
+// src: http://stackoverflow.com/a/563442/4013322
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
 angular.module('dgApp', [])
 .filter('encodeDownload', function() {
     return function (input) {
@@ -17,8 +25,34 @@ angular.module('dgApp', [])
 ]);
 function MainCtrl($scope) {
 
+    $scope.date = '';
+    $scope.startDate = '';
     $scope.calendars = []; // array for users calendars
     $scope.templateEvents = [{}];
+
+    //listen for the file selected event
+    // src: http://stackoverflow.com/a/4950836/4013322
+    $("#choosefile").change(function(e) {
+        var files = e.target.files;
+        var file = files[0];
+        $scope.templateName = file.name;
+        if (!window.FileReader) {
+            alert("No FileReader. Try again using Google Chrome.");
+            return false;
+        }
+        var reader = new FileReader();
+        reader.onload = function() {
+            $scope.templateEvents = JSON.parse(this.result);
+            $scope.$apply();
+        }
+        reader.readAsText(file);
+    });
+
+    $scope.dateDiff = function (d1, d2) {
+        d1 = new Date(d1);
+        d2 = new Date(d2);
+        return Math.ceil((Math.abs(d1.getTime() - d2.getTime())) / (1000 * 3600 * 24));
+    }
 
     $scope.addTemplateEvent = function() {
         $scope.templateEvents.push({}); // add new event to template
@@ -26,7 +60,11 @@ function MainCtrl($scope) {
 
     $scope.deleteTemplateEvent = function(i) {
         if ($scope.templateEvents.length > 1) $scope.templateEvents.splice(i, 1); // don't delete original event (there must be 1)
-    }
+    };
+
+    $scope.saveTemplate = function () {
+        $('#templateModal').modal('hide');
+    };
 
     // src: http://googleappsdeveloper.blogspot.com/2011/12/using-new-js-library-to-unlock-power-of.html
     function handleClientLoad() {
@@ -58,10 +96,67 @@ function MainCtrl($scope) {
         return false;
     }
 
+    $scope.createEventFromTemplate = function(item) {
+        console.log(item);
+        // calculate date
+        var date;
+        if (item.datecalc == 'percent') {
+            var p = parseInt(item.templateDate) / 100;
+            var days = Math.floor(parseInt($scope.dateDiff($scope.date, $scope.startDate)) * p);
+            date = (new Date($scope.startDate)).addDays(days).toJSON().substring(0,10);
+        } else if (item.datecalc == 'afterstart') {
+            date = (new Date($scope.startDate)).addDays(parseInt(item.templateDate)).toJSON().substring(0,10);
+        } else if (item.datecalc == 'beforeend') {
+            date = (new Date($scope.date)).addDays(parseInt(item.templateDate) * -1).toJSON().substring(0,10);
+        }
+        // determine calendar
+        var cal;
+        if (item.defaultCalendar == '1') cal = $scope.calendar;
+        else cal = item.calendarId;
+        var summary = item.summary.replace(/%event/g, $scope.title);
+        console.log(item.summary);
+        var description = item.description.replace(/%event/g, $scope.title);
+        console.log(item.description);
+        var data = {
+            "summary": summary,
+            "description": description,
+            "end": {
+                "date": date
+            },
+            "start": {
+                "date": date
+            },
+            "source": {
+                "url": "http://ericrav.github.io/deadline-generator/"
+            }
+        };
+        // reminders
+        if (item.reminder == "1") {
+            data.reminders = {
+                "overrides": [
+                    {
+                        "method": "email",
+                        "minutes": parseInt(item.reminderDays) * 24 * 60
+                    }
+                ],
+                "useDefault": false
+            }
+        }
+        gapi.client.load('calendar', 'v3', function() {
+            var request = gapi.client.calendar.events.insert({
+                "calendarId": cal,
+                "resource": data
+            });
+            request.execute(function(res) {
+                console.log(res);
+            });
+        });
+    };
+
     $scope.createEvent = function() {
-        console.log($scope.title);
-        console.log($scope.calendar);
-        console.log($scope.date);
+        // console.log($scope.title);
+        // console.log($scope.calendar);
+        // console.log($scope.date);
         var data = {
             "summary": $scope.title,
             "end": {
@@ -83,6 +178,10 @@ function MainCtrl($scope) {
                 console.log(res);
             });
         });
+        for (var i = 0; i < $scope.templateEvents.length; i++) {
+            var item = $scope.templateEvents[i];
+            $scope.createEventFromTemplate(item);
+        }
     }
 
     function makeApiCall() {
